a76d50fcac18fe3ca65ba27bbf0b48e7460b6877
Darrien Rushing <darrien.rushing@selu.edu>
2014-10-20 01:26:05 -0500

    Moved the sample data docs, so the app doesn't crash now.

--- TMP/client/controllers/dashboard.js
+++ TMP/client/controllers/dashboard.js
@@ -1,6 +1,18 @@
 DashboardController = RouteController.extend({
   waitOn: function () {
-  	return Meteor.subscribe('t_hosts');
+  	/*return Meteor.subscribe('t_hosts', function() {
+  		THostsCollection.find({}).observe({
+  			changed: function(id, fields){
+  				Meteor.call("UpdateSeriesData");
+  			},
+  			removed: function(id){
+  				Meteor.call("UpdateSeriesData");
+  			},
+  			added: function(id){
+  				Meteor.call("UpdateSeriesData");
+  			}
+  		});
+  	});*/
   },
 
   data: function () {
