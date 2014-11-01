1c9eca90be9a47c9fe356678f0fc2f465caa691f
Darrien Rushing <darrien.rushing@selu.edu>
2014-11-01 01:26:43 -0500

    Cleaned up upload.js a little.

--- TMP/client/views/upload/upload.js
+++ TMP/client/views/upload/upload.js
@@ -10,6 +10,8 @@ Template.Upload.events({
 
    'change #fileInput': function (event) {
     event.preventDefault();
+    var fileReader = new FileReader(); //used for reading DOM File objects into strings
+
     FS.Utility.eachFile(event, function(file) {
       console.log("file name: " + file.name);
       console.log("file type: " + file.type);
@@ -36,27 +38,23 @@ Template.Upload.events({
       }
       /* XML PARSER */
       if(file.type == "text/xml"){
-        var fileReader = new FileReader();
-
         /* since readAsText() is an asynchronous method, there needs to be a listener
             this event listener is called once the file has finished loading
          */
         fileReader.onload = function(e){
           var xmlText = fileReader.result;
-          //console.log("xmlText: " + xmlText);
-          //parseTheXmlFile(xmlText); //parsing with jQuery
           Meteor.call('parseXML', xmlText, function(err, parsedXml) {
             console.log("parsedXml");
             console.log("px: " + parsedXml);
             console.log("takeupspace");
           });
-
-          //Meteor method call
-          /*Meteor.call('callPy', xmlText, function(err, sumVar){
-            console.log(sumVar);
-          });*/
         }
         fileReader.readAsText(file);
+      }
+
+      /* HTML PARSER */
+      if(file.type == "text/html"){
+
       }
 
     });
@@ -76,18 +74,6 @@ Template.Upload.helpers({
 
 Template.files.files = function() {
   return MimeUpload.find();
-};
-
-parseTheXmlFile = function(xmlText){
-  var xmlDoc = $.parseXML(xmlText);
-  $xml = $(xmlDoc);
-  //console.log("nmaprun: " + console.log($xml.find('nmaprun') ));
-  var services = $xml.find('scaninfo').attr('services');
-  var verboseLevel = $xml.find('verbose').attr('level');
-  var numservices = $xml.find('scaninfo').attr('numservices');
-  console.log("verboseLevel: " + verboseLevel);
-  console.log("numservices: " + numservices);
-  console.log("services: " + services);
 };
 
 /*****************************************************************************/
