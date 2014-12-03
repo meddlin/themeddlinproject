5224b02da58cd259f1838cc1839168125c9b78ae
Darrien Rushing <drushing.dev@gmail.com>
2014-12-03 13:24:43 -0600

    combo host/port view for viewing the ports that are of interest on a certain host

---
+++ client/views/combo_host_port/combo_host_port.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* ComboHostPort: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.ComboHostPort.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.ComboHostPort.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* ComboHostPort: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ComboHostPort.created = function () {
+};
+
+Template.ComboHostPort.rendered = function () {
+};
+
+Template.ComboHostPort.destroyed = function () {
+};
+
+
