05d908b4393b9f0478fba1be94bc7cd16eb51d00
Darrien Rushing <drushing.dev@gmail.com>
2014-12-03 13:23:26 -0600

    PhotoRecon view for viewing photos of target physical locations

---
+++ client/views/photo_recon/photo_recon.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* PhotoRecon: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.PhotoRecon.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.PhotoRecon.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* PhotoRecon: Lifecycle Hooks */
+/*****************************************************************************/
+Template.PhotoRecon.created = function () {
+};
+
+Template.PhotoRecon.rendered = function () {
+};
+
+Template.PhotoRecon.destroyed = function () {
+};
+
+
