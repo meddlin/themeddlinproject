88637607edd1ae74bcf0ac91661c66185a569b61
Darrien Rushing <drushing.dev@gmail.com>
2014-12-03 23:36:21 -0600

    leak view is as developed as it can be currently

---
+++ client/views/leaks/tabular/tabular-leaks.js
@@ -0,0 +1,23 @@
+TabularTables.Leaks = new Tabular.Table({
+	name: "LeakTable",
+	collection: TLeaks,
+	columns: [
+		{data: "leakId", title: "Leak ID"},
+		{data: "description", title: "Description"},
+		{data: "sourceRefs", title: "Source Refs"},
+		{data: "leakType", title: "Leak Type"},
+		{data: "title", title: "Title"},
+		{data: "importDate", title: "Import Date"},
+		{data: "leakDate", title: "Leak Date"},
+		{data: "attackers", title: "Attackers"},
+		{data: "numEntries", title: "Num Entries"},
+		{data: "score", title: "Score"},
+		{data: "numDomainsAffected", title: "Num Dom Aff."},
+		{data: "attackMethod", title: "Attack Method"},
+		{data: "targetIndustries", title: "Target Industries"},
+		{data: "passwordHash", title: "Pass Hash"},
+		{data: "targets", title: "Targets"},
+		{data: "mediaRefs", title: "Media Refs"},
+		{data: "module", title: "Module"}
+	]
+});
